6b9bbaabafd3eba779880f82f70038a123499949
L3to <lf2006@hotmail.com.br>
2025-05-14 01:30:21 -0300

    feat: atualiza configuração do banco de dados e melhora tratamento de erros
    - Altera a URI do banco de dados para usar o host correto.
    - Adiciona verificação do formato dos dados recebidos na rota de registro de discos.
    - Implementa tratamento de exceções para campos obrigatórios e erros inesperados.
    - Organiza a importação do módulo de configuração.

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = (
-        "postgres://discomania:postgres@localhost:5432/bddiscomania"
+        "postgresql://discomania:postgres@postgres555591:5432/bddiscomania"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- docker_api.py
+++ docker_api.py
@@ -1,6 +1,7 @@
-from config import Config
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+from config import Config
 from models import Discos, db
 
 app = Flask(__name__)
@@ -13,15 +14,34 @@ with app.app_context():
     db.create_all()
 
 
+def get_last_id():
+    last_disco = Discos.query.order_by(Discos.id.desc()).first()
+    return last_disco.id if last_disco else 0
+
+
 @app.route("/discos", methods=["POST"])
 def registrar_disco():
-    dados = request.json
-    novo_disco = Discos(
-        nome=dados["nome"],
-        autor=dados["autor"],
-        ano=dados["ano"],
-        genero=dados["genero"],
-    )
+    dados = request.get_json()
+
+    if not isinstance(dados, dict):
+        return jsonify({"error": "Invalid data format. Expected a JSON object."}), 400
+
+    lastid = get_last_id()
+    try:
+        novo_disco = Discos(
+            id=lastid + 1,
+            nome=dados.get("nome"),
+            autor=dados.get("autor"),
+            ano=dados.get("ano"),
+            genero=dados.get("genero"),
+        )
+    except KeyError as e:
+        return jsonify({"error": f"Missing required field: {e}"}), 400
+    except TypeError as e:
+        return jsonify({"error": f"Type error: {e}"}), 400
+    except Exception as e:
+        return jsonify({"error": f"An unexpected error occurred: {e}"}), 500
+
     db.session.add(novo_disco)
     db.session.commit()
     return jsonify(novo_disco.to_dict()), 201
@@ -42,7 +62,7 @@ def pegar_disco(id):
 @app.route("/discos/<int:id>", methods=["PUT"])
 def atualizar_disco(id):
     discos = Discos.query.get_or_404(id)
-    dados = request.json
+    dados = request.get_json()
     discos.nome = dados.get("nome", discos.nome)
     discos.autor = dados.get("autor", discos.autor)
     discos.ano = dados.get("ano", discos.ano)
@@ -60,4 +80,4 @@ def deletar_disco(id):
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
